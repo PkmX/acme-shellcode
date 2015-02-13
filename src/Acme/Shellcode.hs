4ddec60e71981ba08d745fcffad9daed248aeb50
PkmX <pkmx.tw@gmail.com>
2015-02-14 00:34:16 +0800

    Use ByteString and reject invalid inputs

--- src/Acme/Shellcode.hs
+++ src/Acme/Shellcode.hs
@@ -2,23 +2,25 @@
 
 module Acme.Shellcode (shell, shellQQ) where
 
+import Control.Applicative
+import Data.ByteString.Char8
 import Data.Char
 import Foreign.C
 import Foreign.Ptr
 import Language.Haskell.TH
 import Language.Haskell.TH.Quote
-import Text.Parsec
+import Text.Parsec hiding (many)
 import Text.Parsec.Language
 import Text.Parsec.String
 import Text.Parsec.Token
 
 foreign import ccall "shellcode.h shellcode" c_shellcode :: Ptr () -> CSize -> IO ()
 
-parser :: Parser String
-parser = many [ chr $ fromIntegral n | n <- integer haskell, 0 <= n && n <= 255 ]
+parser :: Parser ByteString
+parser = pack <$> many [ chr $ fromIntegral n | n <- integer haskell, 0 <= n && n <= 255 ] <* eof
 
-shell :: String -> Q Exp
-shell s = [e|withCAStringLen $(stringE s) $ \(buf, len) -> c_shellcode (castPtr buf) (fromIntegral len)|]
+shell :: ByteString -> Q Exp
+shell s = [e|useAsCStringLen $(stringE $ unpack s) $ \(buf, len) -> c_shellcode (castPtr buf) (fromIntegral len)|]
 
 shellQQ :: QuasiQuoter
 shellQQ = QuasiQuoter { quoteExp = either (fail . show) shell . parse parser ""
